608d3f0afb76b8ab6d34a4848437a0bfc934ca3b
jslpdcharan <59231349+jslpdcharan@users.noreply.github.com>
2025-02-12 23:57:21 -0600

    Initial Commit

---
+++ NotesApp/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp
+//
+//  Created by Satyanarayana on 12/02/25.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NotesAppTests/NotesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppTests.swift
+//  NotesAppTests
+//
+//  Created by Satyanarayana on 12/02/25.
+//
+
+import Testing
+@testable import NotesApp
+
+struct NotesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
